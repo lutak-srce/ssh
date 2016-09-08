84df11aeeaa33f218ceb1b8d9a3d69d4c93aec9b
Jakov Sosic <jsosic@gmail.com>
2016-09-08 16:55:16 +0200

    2016-09 big sync from bitbucket (#1)

--- manifests/server/keys.pp
+++ manifests/server/keys.pp
@@ -4,79 +4,100 @@
 # and is only intended to use as ssh::server
 # helper class
 #
-class ssh::server::keys {
-  # ssh keys
-  file { '/etc/ssh/ssh_host_dsa_key':
+class ssh::server::keys (
+  $private_mode  = $::ssh::params::private_mode,
+  $private_group = $::ssh::params::private_group,
+) inherits ssh::params {
+
+  $keys_dir = $::ssh::server::keys_dir
+
+  File {
     ensure  => file,
     owner   => root,
     group   => root,
-    mode    => '0600',
-    source  => [
+    mode    => '0644',
+    require => Package['openssh-server'],
+    notify  => Service['sshd'],
+  }
+
+  # DSA key
+  file { '/etc/ssh/ssh_host_dsa_key':
+    group  => $private_group,
+    mode   => $private_mode,
+    source => [
       'puppet:///private/ssh/ssh_host_dsa_key',
       'puppet:///modules/ssh/ssh_host_dsa_key',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
   }
   file { '/etc/ssh/ssh_host_dsa_key.pub':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => [
+    source => [
       'puppet:///private/ssh/ssh_host_dsa_key.pub',
       'puppet:///modules/ssh/ssh_host_dsa_key.pub',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
   }
+
+  # RSA1 key
   file { '/etc/ssh/ssh_host_key':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    source  => [
+    group  => $private_group,
+    mode   => $private_mode,
+    source => [
       'puppet:///private/ssh/ssh_host_key',
       'puppet:///modules/ssh/ssh_host_key',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
   }
   file { '/etc/ssh/ssh_host_key.pub':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => [
+    source => [
       'puppet:///private/ssh/ssh_host_key.pub',
       'puppet:///modules/ssh/ssh_host_key.pub',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
   }
+
+  # RSA key
   file { '/etc/ssh/ssh_host_rsa_key':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0600',
-    source  => [
+    group  => $private_group,
+    mode   => $private_mode,
+    source => [
       'puppet:///private/ssh/ssh_host_rsa_key',
       'puppet:///modules/ssh/ssh_host_rsa_key',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
   }
   file { '/etc/ssh/ssh_host_rsa_key.pub':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    source  => [
+    source => [
       'puppet:///private/ssh/ssh_host_rsa_key.pub',
       'puppet:///modules/ssh/ssh_host_rsa_key.pub',
     ],
-    require => Package['openssh-server'],
-    notify  => Service['sshd'],
+  }
+
+  # ECDSA key
+  file { '/etc/ssh/ssh_host_ecdsa_key':
+    group  => $private_group,
+    mode   => $private_mode,
+    source => [
+      'puppet:///private/ssh/ssh_host_ecdsa_key',
+      'puppet:///modules/ssh/ssh_host_ecdsa_key',
+    ],
+  }
+  file { '/etc/ssh/ssh_host_ecdsa_key.pub':
+    source => [
+      'puppet:///private/ssh/ssh_host_ecdsa_key.pub',
+      'puppet:///modules/ssh/ssh_host_ecdsa_key.pub',
+    ],
+  }
+
+  # ED25519 key
+  file { '/etc/ssh/ssh_host_ed25519_key':
+    group  => $private_group,
+    mode   => $private_mode,
+    source => [
+      'puppet:///private/ssh/ssh_host_ed25519_key',
+      'puppet:///modules/ssh/ssh_host_ed25519_key',
+    ],
+  }
+  file { '/etc/ssh/ssh_host_ed25519_key.pub':
+    source => [
+      'puppet:///private/ssh/ssh_host_ed25519_key.pub',
+      'puppet:///modules/ssh/ssh_host_ed25519_key.pub',
+    ],
   }
 
 }
